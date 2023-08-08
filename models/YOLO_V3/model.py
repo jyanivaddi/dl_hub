efaeb31d8e654881bc64efaf5636046d14e3370f
jyanivaddi <135302082+jyanivaddi@users.noreply.github.com>
2023-08-09 01:41:10 +0530

    little things

--- models/YOLO_V3/model.py
+++ models/YOLO_V3/model.py
@@ -164,13 +164,13 @@ class YOLOv3(nn.Module):
         return layers
 
 
-if __name__ == "__main__":
-    num_classes = 20
-    IMAGE_SIZE = 416
-    model = YOLOv3(num_classes=num_classes)
-    x = torch.randn((2, 3, IMAGE_SIZE, IMAGE_SIZE))
-    out = model(x)
-    assert model(x)[0].shape == (2, 3, IMAGE_SIZE//32, IMAGE_SIZE//32, num_classes + 5)
-    assert model(x)[1].shape == (2, 3, IMAGE_SIZE//16, IMAGE_SIZE//16, num_classes + 5)
-    assert model(x)[2].shape == (2, 3, IMAGE_SIZE//8, IMAGE_SIZE//8, num_classes + 5)
-    print("Success!")
+#if __name__ == "__main__":
+#    num_classes = 20
+#    IMAGE_SIZE = 416
+#    model = YOLOv3(num_classes=num_classes)
+#    x = torch.randn((2, 3, IMAGE_SIZE, IMAGE_SIZE))
+#    out = model(x)
+#    assert model(x)[0].shape == (2, 3, IMAGE_SIZE//32, IMAGE_SIZE//32, num_classes + 5)
+#    assert model(x)[1].shape == (2, 3, IMAGE_SIZE//16, IMAGE_SIZE//16, num_classes + 5)
+#    assert model(x)[2].shape == (2, 3, IMAGE_SIZE//8, IMAGE_SIZE//8, num_classes + 5)
+#    print("Success!")
