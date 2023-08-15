c86c4cff89096c3211d975d9a7b34fe7bba9277f
jyanivaddi <135302082+jyanivaddi@users.noreply.github.com>
2023-08-15 22:57:05 +0530

    Update config.py

--- YOLO_V3/config.py
+++ YOLO_V3/config.py
@@ -8,7 +8,8 @@ from albumentations.pytorch import ToTensorV2
 # For Gdrive
 #DATASET = '/content/gdrive/MyDrive/Datasets/PASCAL_VOC_SMALL'
 # For Kaggle
-DATASET = '/kaggle/input/pascal-voc-dataset-used-in-yolov3-video/PASCAL_VOC'
+#DATASET = '/kaggle/input/pascal-voc-dataset-used-in-yolov3-video/PASCAL_VOC'
+DATASET = '/home/e183534/PASCAL_VOC'
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 #DEVICE = "cpu"
 # seed_everything()  # If you want deterministic behavior
