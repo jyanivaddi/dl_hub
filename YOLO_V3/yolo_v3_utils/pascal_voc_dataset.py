bf8c66aa43ff8e8596fc43bb0ae64f391660c90c
Jyani Vaddi <Jyani.Vaddi@gmail.com>
2023-08-15 16:51:28 +0530

    reorg

--- YOLO_V3/yolo_v3_utils/pascal_voc_dataset.py
+++ YOLO_V3/yolo_v3_utils/pascal_voc_dataset.py
@@ -89,37 +89,37 @@ class YOLODataset(Dataset):
         return image, tuple(targets)
 
 
-def test():
-    anchors = config.ANCHORS
-
-    transform = config.test_transforms
-
-    dataset = YOLODataset(
-        "COCO/train.csv",
-        "COCO/images/images/",
-        "COCO/labels/labels_new/",
-        S=[13, 26, 52],
-        anchors=anchors,
-        transform=transform,
-    )
-    S = [13, 26, 52]
-    scaled_anchors = torch.tensor(anchors) / (
-        1 / torch.tensor(S).unsqueeze(1).unsqueeze(1).repeat(1, 3, 2)
-    )
-    loader = DataLoader(dataset=dataset, batch_size=1, shuffle=True)
-    for x, y in loader:
-        boxes = []
-
-        for i in range(y[0].shape[1]):
-            anchor = scaled_anchors[i]
-            print(anchor.shape)
-            print(y[i].shape)
-            boxes += cells_to_bboxes(
-                y[i], is_preds=False, S=y[i].shape[2], anchors=anchor
-            )[0]
-        boxes = nms(boxes, iou_threshold=1, threshold=0.7, box_format="midpoint")
-        print(boxes)
-        plot_image(x[0].permute(1, 2, 0).to("cpu"), boxes)
+#def test():
+#    anchors = config.ANCHORS
+#
+#    transform = config.test_transforms
+#
+#    dataset = YOLODataset(
+#        "COCO/train.csv",
+#        "COCO/images/images/",
+#        "COCO/labels/labels_new/",
+#        S=[13, 26, 52],
+#        anchors=anchors,
+#        transform=transform,
+#    )
+#    S = [13, 26, 52]
+#    scaled_anchors = torch.tensor(anchors) / (
+#        1 / torch.tensor(S).unsqueeze(1).unsqueeze(1).repeat(1, 3, 2)
+#    )
+#    loader = DataLoader(dataset=dataset, batch_size=1, shuffle=True)
+#    for x, y in loader:
+#        boxes = []
+#
+#        for i in range(y[0].shape[1]):
+#            anchor = scaled_anchors[i]
+#            print(anchor.shape)
+#            print(y[i].shape)
+#            boxes += cells_to_bboxes(
+#                y[i], is_preds=False, S=y[i].shape[2], anchors=anchor
+#            )[0]
+#        boxes = nms(boxes, iou_threshold=1, threshold=0.7, box_format="midpoint")
+#        print(boxes)
+#        plot_image(x[0].permute(1, 2, 0).to("cpu"), boxes)
 
 
 #if __name__ == "__main__":
